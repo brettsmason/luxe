de9cc8beab6e78bc4abd41a7ad85b3bffdf8a526
Brett Mason <brettsmason@gmail.com>
2018-04-30 10:28:37 +0100

    add new get_global_hierarchy function from HC

--- resources/views/index.php
+++ resources/views/index.php
@@ -4,14 +4,14 @@
 
 	<body <?php Hybrid\attr( 'body' ) ?>>
 		<div class="app">
-			<?php Hybrid\render_view( 'header', Hybrid\get_template_hierarchy() ) // Load header/* template. ?>
+			<?php Hybrid\render_view( 'header', Hybrid\get_global_hierarchy() ) // Load header/* template. ?>
 
 			<div class="app-content">
-				<?php Hybrid\render_view( 'content', Hybrid\get_template_hierarchy() ) // Load content/* template. ?>
+				<?php Hybrid\render_view( 'content', Hybrid\get_global_hierarchy() ) // Load content/* template. ?>
 				<?php Hybrid\render_view( 'sidebar', 'primary', [ 'name' => 'primary' ] ) ?>
 			</div>
 
-			<?php Hybrid\render_view( 'footer', Hybrid\get_template_hierarchy() ) // Load footer/* template. ?>
+			<?php Hybrid\render_view( 'footer', Hybrid\get_global_hierarchy() ) // Load footer/* template. ?>
 		</div><!-- .app -->
 
 		<?php wp_footer() ?>

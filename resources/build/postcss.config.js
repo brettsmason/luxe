b2ad4b44b7fbd35441258a15e40bf35ba5f2630f
Brett Mason <brettsmason@gmail.com>
2018-05-03 20:41:13 +0100

    Remove px fallback

--- resources/build/postcss.config.js
+++ resources/build/postcss.config.js
@@ -4,7 +4,11 @@
 module.exports = ({ file, options, env }) => ({
 	plugins: {
 		'postcss-import': {},
-		'postcss-cssnext': {},
+		'postcss-cssnext': {
+			features: {
+				rem: false
+			}
+		},
 		autoprefixer: env === 'production',
 		cssnano: env === 'production'
 	}

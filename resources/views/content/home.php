31fb23d60ef57e2b068a8da1efa019c9901b27f0
Brett Mason <brettsmason@gmail.com>
2018-05-11 12:12:54 +0100

    Add blog view with 3 column grid

---
+++ resources/views/content/home.php
@@ -0,0 +1,14 @@
+<main id="main" class="app-main grid grid--3">
+	<?php Hybrid\render_view( 'partials', 'archive-header' ) ?>
+
+	<?php if ( have_posts() ) : ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php Hybrid\render_view( 'entry/archive', Hybrid\get_post_hierarchy() ) ?>
+
+		<?php endwhile ?>
+
+		<?php Hybrid\render_view( 'partials', 'pagination-posts' ) ?>
+
+	<?php endif ?>
+</main>

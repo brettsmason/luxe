f452466224f96ecc85e318765eea1e0a3d8cdce8
Brett Mason <brettsmason@gmail.com>
2018-05-11 15:12:51 +0100

    Add auto width grid option

--- resources/views/sidebar/subsidiary.php
+++ resources/views/sidebar/subsidiary.php
@@ -2,6 +2,6 @@
 if ( ! is_active_sidebar( $data->name ) ) return;
 ?>
 
-<aside class="sidebar sidebar--<?= esc_attr( $data->name ) ?> wrapper">
+<aside class="sidebar sidebar--<?= esc_attr( $data->name ) ?> wrapper grid grid--auto">
 	<?php dynamic_sidebar( $data->name ) ?>
 </aside>

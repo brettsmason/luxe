df986dc785fa75a87bc15bc2cdded6201ff83f88
Brett Mason <brettsmason@gmail.com>
2018-05-29 16:28:32 +0100

    Get image min working and tidy up the file along with comments.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,68 +1,81 @@
-let mix = require('laravel-mix');
+const { mix } = require('laravel-mix');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for your application, as well as bundling up your JS files.
- |
- */
+// Set path to our generated assets.
+mix.setPublicPath('public');
 
-mix
-	.setPublicPath('public')
-	.js('resources/scripts/app.js', 'public/scripts')
-	.sass('resources/styles/screen.scss', 'public/styles')
-	.sourceMaps()
-	.copy('resources/img/*.{jpg,jpeg,png,gif}', 'public/img', false)
-	.copy('resources/svg/*.svg', 'public/svg', false)
-	.copy('resources/fonts/**/*.{svg,eot,ttf,woff,woff2}', 'public/fonts', false)
-	.options({})
-	.browserSync({
-		proxy: 'theme-development.localhost',
-		files: [
-			"public/**/*.{jpg,jpeg,png,gif,svg,eot,ttf,woff,woff2}",
-			"resources/views/**/*.php",
-			"app/**/*.php"
-		]
-	});
+// Compile assets.
+mix.js('resources/scripts/app.js', 'public/scripts').sourceMaps();
+mix.sass('resources/styles/screen.scss', 'public/styles').sourceMaps();
 
-// Full API
-// mix.js(src, output);
-// mix.react(src, output); <-- Identical to mix.js(), but registers React Babel compilation.
-// mix.preact(src, output); <-- Identical to mix.js(), but registers Preact compilation.
-// mix.coffee(src, output); <-- Identical to mix.js(), but registers CoffeeScript compilation.
-// mix.ts(src, output); <-- TypeScript support. Requires tsconfig.json to exist in the same folder as webpack.mix.js
-// mix.extract(vendorLibs);
-// mix.sass(src, output);
-// mix.standaloneSass('src', output); <-- Faster, but isolated from Webpack.
-// mix.fastSass('src', output); <-- Alias for mix.standaloneSass().
-// mix.less(src, output);
-// mix.stylus(src, output);
-// mix.postCss(src, output, [require('postcss-some-plugin')()]);
-// mix.browserSync('my-site.test');
-// mix.combine(files, destination);
-// mix.babel(files, destination); <-- Identical to mix.combine(), but also includes Babel compilation.
-// mix.copy(from, to);
-// mix.copyDirectory(fromDir, toDir);
-// mix.minify(file);
-// mix.sourceMaps(); // Enable sourcemaps
-// mix.version(); // Enable versioning.
-// mix.disableNotifications();
-// mix.setPublicPath('path/to/public');
-// mix.setResourceRoot('prefix/for/resource/locators');
-// mix.autoload({}); <-- Will be passed to Webpack's ProvidePlugin.
-// mix.webpackConfig({}); <-- Override webpack.config.js, without editing the file directly.
-// mix.babelConfig({}); <-- Merge extra Babel configuration (plugins, etc.) with Mix's default.
-// mix.then(function () {}) <-- Will be triggered each time Webpack finishes building.
-// mix.extend(name, handler) <-- Extend Mix's API with your own components.
-// mix.options({
-//   extractVueStyles: false, // Extract .vue component styling to file, rather than inline.
-//   globalVueStyles: file, // Variables file to be imported in every component.
-//   processCssUrls: true, // Process/optimize relative stylesheet url()'s. Set to false, if you don't want them touched.
-//   purifyCss: false, // Remove unused CSS selectors.
-//   uglify: {}, // Uglify-specific options. https://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
-//   postCss: [] // Post-CSS options: https://github.com/postcss/postcss/blob/master/docs/plugins.md
-// });
+// Generate a manifest file for cache busting.
+// Append a unique hash for production only assets.
+if(mix.inProduction()) {
+	mix.version();
+}
+
+// Add our own Webpack config options.
+// Here we are using CopyWebpackPlugin rather than the `.copy` mix method
+// as we want to minimize images too, and mix doesn't currently have a
+// built in method to handle this.
+mix.webpackConfig({
+  stats: 'minimal',
+  performance: {
+    hints: false
+  },
+  plugins: [
+    // Copy our static assets to the public directory.
+    new CopyWebpackPlugin([
+      {
+        from: 'resources/img',
+        to: 'img',
+      },
+      {
+        from: 'resources/svg',
+        to: 'svg',
+      },
+      {
+        from: 'resources/fonts',
+        to: 'fonts',
+      }
+    ]),
+    // Minify and clean up our image assets.
+    new ImageminPlugin({
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      disable: process.env.NODE_ENV !== 'production',
+      optipng: {
+        optimizationLevel: 3
+      },
+      gifsicle: {
+        optimizationLevel: 3
+      },
+      pngquant: {
+        quality: '65-90',
+        speed: 4
+      },
+      svgo: {
+        plugins: [
+          { removeUnknownsAndDefaults: false },
+          { cleanupIDs: false },
+          { removeViewBox: false }
+        ]
+      },
+      plugins: [imageminMozjpeg({ quality: 75 })]
+    })
+  ]
+});
+
+// monitor files for changes and inject your changes into the browser.
+mix.browserSync({
+  proxy: 'theme-development.localhost',
+  files: [
+    "public/**/*.{jpg,jpeg,png,gif,svg,eot,ttf,woff,woff2}",
+    "resources/views/**/*.php",
+    "app/**/*.php"
+  ]
+});
+
+// Disable processing asset URLs in Sass files.
+mix.options({ processCssUrls: false });

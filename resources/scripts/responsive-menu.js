eb96e98cef1fdb9c62f8af53d5ddd61f10a0fd5b
Brett Mason <brettsmason@gmail.com>
2018-06-06 21:02:34 +0100

    Start of height calculation of submenus

--- resources/scripts/responsive-menu.js
+++ resources/scripts/responsive-menu.js
@@ -66,6 +66,22 @@ export default class ResponsiveMenu {
 				}
 			}
 		});
+
+		let toggles = document.getElementsByClassName('menu__submenu-toggle');
+		console.log(toggles);
+
+			[...toggles].forEach(toggle => {
+				toggle.addEventListener('click', function(e) {
+				let submenu = toggle.nextElementSibling;
+				let isCollapsed = toggle.getAttribute('aria-expanded') === 'true';
+
+				if(isCollapsed) {
+					this._expandSection(submenu);
+				} else {
+					this._collapseSection(submenu);
+				}
+			});
+		});
 	}
 
 	// Toggle aria-expanded state when button is pressed
@@ -178,5 +194,51 @@ export default class ResponsiveMenu {
 
 		var uuid = s.join('');
 		return uuid;
+	}
+
+	_collapseSection(element) {
+		// get the height of the element's inner content, regardless of its actual size
+		var sectionHeight = element.scrollHeight;
+
+		// temporarily disable all css transitions
+		var elementTransition = element.style.transition;
+		element.style.transition = '';
+
+		// on the next frame (as soon as the previous style change has taken effect),
+		// explicitly set the element's height to its current pixel height, so we
+		// aren't transitioning out of 'auto'
+		requestAnimationFrame(function() {
+		  element.style.height = sectionHeight + 'px';
+		  element.style.transition = elementTransition;
+
+		  // on the next frame (as soon as the previous style change has taken effect),
+		  // have the element transition to height: 0
+		  requestAnimationFrame(function() {
+			element.style.height = 0 + 'px';
+		  });
+		});
+
+		// mark the section as "currently collapsed"
+		element.setAttribute('data-collapsed', 'true');
+	}
+
+	_expandSection(element) {
+		// get the height of the element's inner content, regardless of its actual size
+		var sectionHeight = element.scrollHeight;
+
+		// have the element transition to the height of its inner content
+		element.style.height = sectionHeight + 'px';
+
+		// when the next css transition finishes (which should be the one we just triggered)
+		element.addEventListener('transitionend', function(e) {
+		  // remove this event listener so it only gets triggered once
+		  element.removeEventListener('transitionend', arguments.callee);
+
+		  // remove "height" from the element's inline styles, so it can return to its initial value
+		  element.style.height = null;
+		});
+
+		// mark the section as "currently not collapsed"
+		element.setAttribute('data-collapsed', 'false');
 	}
 }

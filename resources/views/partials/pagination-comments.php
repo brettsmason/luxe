a14c9e9b5e21071dd8d8eab1eb2fd34879e3a5c0
Brett Mason <brettsmason@gmail.com>
2018-06-27 12:36:30 +0100

    Remove not needed pagination args

--- resources/views/partials/pagination-comments.php
+++ resources/views/partials/pagination-comments.php
@@ -3,6 +3,4 @@ Hybrid\render_pagination( 'comments', [
 	'prev_text'       => __( '&larr; Previous', 'luxe' ),
 	'next_text'       => __( 'Next &rarr;', 'luxe' ),
 	'title_text'      => __( 'Comments Navigation' ),
-	'container_class' => 'pagination pagination--comments',
-	'title_class'     => 'pagination__title screen-reader-text'
 ] );
--- resources/views/partials/pagination-singular.php
+++ resources/views/partials/pagination-singular.php
@@ -1,8 +1,6 @@
 <?php
 Hybrid\render_pagination( 'singular', [
-	'context'         => 'singular',
 	'show_all'        => true,
 	'prev_next'       => false,
 	'title_text'      => __( 'Pages:', 'luxe' ),
-	'container_class' => 'pagination pagination--singular',
 ] );

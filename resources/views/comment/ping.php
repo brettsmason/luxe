b1e1b02f608604913ce1f7d9db676c72610b131d
Brett Mason <brettsmason@gmail.com>
2018-06-27 11:06:36 +0100

    Make ping templates match comments

--- resources/views/comment/ping.php
+++ resources/views/comment/ping.php
@@ -1,7 +1,8 @@
 <li <?php Hybrid\attr( 'comment' ) ?>>
 
 	<div class="comment__meta">
-		<span class="comment__author"><?php comment_author_link() ?></span><br />
+		<span class="comment__author"><?php comment_author_link() ?></span>
+		<?= Luxe\get_meta_sep() ?>
 		<?php /* translators: %s how many days ago. */ ?>
 		<a href="<?php comment_link() ?>" class="comment__permalink"><time class="comment__published"><?php printf( __( '%s ago', 'luxe' ), human_time_diff( get_comment_time( 'U' ), current_time( 'timestamp' ) ) ) ?></time></a>
 		<?php edit_comment_link( null, Luxe\get_meta_sep() ) ?>

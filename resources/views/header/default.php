8aa509adc0f7eb383226518159403ee822863cd6
Brett Mason <brettsmason@gmail.com>
2018-06-28 15:35:29 +0100

    Bunch of utility class additions (still not 100% sure on these yet...) and some needed mixins.

--- resources/views/header/default.php
+++ resources/views/header/default.php
@@ -1,11 +1,13 @@
-<header class="app-header wrapper">
-	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'luxe' ) ?></a>
+<header class="app-header u-px-1 u-py-1">
+	<div class="wrapper wrapper--wide u-flex u-items-center u-space-between">
+		<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'luxe' ) ?></a>
 
-	<div class="app-header__branding">
-		<?php the_custom_logo() ?>
-		<?php Hybrid\site_title( '', [ 'class' => 'app-header__title u-h4' ] ) ?>
-		<?php Hybrid\site_description( '', [ 'class' => 'app-header__description' ] ) ?>
+		<div class="app-header__branding">
+			<?php the_custom_logo() ?>
+			<?php Hybrid\site_title( '', [ 'class' => 'app-header__title u-h4' ] ) ?>
+			<?php Hybrid\site_description( '', [ 'class' => 'app-header__description' ] ) ?>
+		</div>
+
+		<?php Hybrid\render_view( 'menu', 'primary', [ 'name' => 'primary' ] ) ?>
 	</div>
-
-	<?php Hybrid\render_view( 'menu', 'primary', [ 'name' => 'primary' ] ) ?>
 </header>

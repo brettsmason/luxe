9a00480a4424e7050d3368ef4f62c2c9800e3828
Brett Mason <brettsmason@gmail.com>
2018-08-31 09:09:07 +0100

    Add better theme support for WooCommerce.

--- app/functions-woocommerce.php
+++ app/functions-woocommerce.php
@@ -1,18 +1,74 @@
 <?php
 /**
- * WooCommerce specific functions.
+ * WooCommerce integration.
  *
- * @package Luxe
+ * This file integrates the theme with WooCommerce.
+ *
+ * @package    Luxe
+ * @subpackage Includes
+ * @author     Brett Mason <brettsmason@gmail.com>
+ * @copyright  Copyright (c) 2018, Brett Mason
+ * @link       https://github.com/brettsmason/luxe/
+ * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
-namespace Luxe;
+namespace Mythic;
+
+use function Hybrid\Template\path;
 
 /**
- * Put WooCommerce templates under resources/views/woocommerce.
+ * Adds theme support for the WooCommerce plugin.
  *
- * @param string The path to directory.
+ * @since  1.0.0
+ * @access public
+ * @return void
+ */
+add_action( 'after_setup_theme', function() {
+
+	add_theme_support( 'woocommerce', [
+		'thumbnail_image_width' => 150,
+		'single_image_width'    => 300,
+
+        'product_grid'          => [
+            'default_rows'    => 3,
+            'min_rows'        => 2,
+            'max_rows'        => 8,
+            'default_columns' => 4,
+            'min_columns'     => 2,
+            'max_columns'     => 5,
+		],
+	] );
+} );
+
+/**
+ * This overrides the top-level WooCommerce templates that would normally go in
+ * the theme root. By default, we're looking for a `resources/views/woocommerce.php`
+ * template, which falls back to `resources/views/index.php`.
+ *
+ * @since  1.0.0
+ * @access public
+ * @param  array  $files
+ * @return array
+ */
+add_filter( 'woocommerce_template_loader_files', function( $files ) {
+
+	return [
+		path( 'woocommerce.php' ),
+		path( 'index.php' )
+	];
+
+}, PHP_INT_MAX );
+
+/**
+ * Filters the path to the `woocommerce` template parts folder.  This filter
+ * moves that folder to `resources/views/woocommerce`.
+ *
+ * @since  1.0.0
+ * @access public
+ * @param  string  $path
  * @return string
  */
 add_filter( 'woocommerce_template_path', function( $path ) {
-	return \Hybrid\Template\path( $path );
+
+	return path( $path );
 } );
---
+++ resources/views/content/woocommerce.php
@@ -0,0 +1,3 @@
+<main id="main" class="app-main">
+	<?php woocommerce_content() ?>
+</main>

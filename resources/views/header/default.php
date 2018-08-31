ee47960b04e9fca9c95cf40fecb8f3754a793219
Brett Mason <brettsmason@gmail.com>
2018-08-31 12:33:59 +0100

    Better screen-reader-text styles

--- resources/views/header/default.php
+++ resources/views/header/default.php
@@ -1,7 +1,7 @@
 <header class="app-header">
-	<div class="app-header__wrapper wrapper wrapper--wide">
-		<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'luxe' ) ?></a>
+	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'luxe' ) ?></a>
 
+	<div class="app-header__wrapper wrapper wrapper--wide">
 		<div class="app-header__branding">
 			<?php the_custom_logo() ?>
 			<?php Hybrid\Site\display_title( [ 'class' => 'app-header__title u-h4' ] ) ?>

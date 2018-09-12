41d4cd57f42b18f1b15585e31853dbdc5b75f429
Brett Mason <brettsmason@gmail.com>
2018-09-12 20:48:44 +0100

    Add styling for the various WC gallery options as well as the my account section.

--- app/functions-woocommerce.php
+++ app/functions-woocommerce.php
@@ -44,8 +44,8 @@ add_action( 'after_setup_theme', function() {
 		],
 	] );
 
-	// add_theme_support( 'wc-product-gallery-zoom' );
-    // add_theme_support( 'wc-product-gallery-lightbox' );
+	add_theme_support( 'wc-product-gallery-zoom' );
+    add_theme_support( 'wc-product-gallery-lightbox' );
     add_theme_support( 'wc-product-gallery-slider' );
 } );
 

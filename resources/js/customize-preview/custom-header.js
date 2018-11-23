6427a7aff1592612a6b19582e000300dc7730052
Brett Mason <brettsmason@gmail.com>
2018-11-23 16:34:44 +0000

    Fix some JS errors.

--- resources/js/customize-preview/custom-header.js
+++ resources/js/customize-preview/custom-header.js
@@ -7,28 +7,29 @@
  * @link      https://github.com/brettsmason/luxe
  * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
-export default function () {
+export default function() {
+
   // Site title.
-  wp.customize('blogname', (value) => {
-    value.bind((to) => {
-      document.querySelector('.app-header__title a').textContent = to;
+  wp.customize( 'blogname', ( value ) => {
+    value.bind( ( to ) => {
+      document.querySelector( '.app-header__title a' ).textContent = to;
     });
   });
 
   // Site description.
-  wp.customize('blogdescription', (value) => {
-    value.bind((to) => {
-      document.querySelector('.app-header__description').textContent = to;
+  wp.customize( 'blogdescription', ( value ) => {
+    value.bind( ( to ) => {
+      document.querySelector( '.app-header__description' ).textContent = to;
     });
   });
 
   // Header text color.
-  wp.customize('header_textcolor', (value) => {
-    value.bind((to) => {
-      const headerItems = document.querySelectorAll('.app-header__title a, .app-header__description');
+  wp.customize( 'header_textcolor', ( value ) => {
+    value.bind( ( to ) => {
+      const headerItems = document.querySelectorAll( '.app-header__title a, .app-header__description' );
 
-      headerItems.forEach((text) => {
-        if (to === 'blank') {
+      headerItems.forEach( ( text ) => {
+        if ( 'blank' === to ) {
           text.style.clip = 'rect(0 0 0 0)';
           text.style.position = 'absolute';
         } else {

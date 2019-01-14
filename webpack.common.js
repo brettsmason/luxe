0cb0698a7e8ef8957fd39c00ee42308f1cef6433
Brett Mason <brettsmason@gmail.com>
2019-01-14 09:13:57 +0000

    Remove variable things for now. Still havent got a working version.

--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,6 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
-const jsonImporter = require('node-sass-json-importer');
 const ManifestPlugin = require('webpack-manifest-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const isProduction = process.env.NODE_ENV === 'production';
@@ -76,8 +75,7 @@ module.exports = {
 						loader: 'sass-loader',
 						options: {
 							sourceMap: ! isProduction,
-							outputStyle: 'expanded',
-							importer: jsonImporter()
+							outputStyle: 'expanded'
 						}
 					}
 				]

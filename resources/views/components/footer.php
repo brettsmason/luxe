8ccee52dff91a4cc5b893fc513bb31c78517f0f3
Brett Mason <brettsmason@gmail.com>
2019-06-27 20:29:01 +0100

    Footer copyright change

--- resources/views/components/footer.php
+++ resources/views/components/footer.php
@@ -1,6 +1,6 @@
 <footer class="app-footer">
 	<div class="app-footer__wrapper">
-		<p class="app-footer__credit">
+		<p class="app-footer__copyright">
 			<?php
 			printf(
 				esc_html__( 'Copyright &#169; %1$s %2$s', 'luxe' ),
